d615dee3a7b598c09c1a5e88c2651506ef90b06c
Harry <harrychenhc@icloud.com>
2021-02-24 11:30:32 -0500

    typing the app to be a NestExpressApplication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/test-route')
+  getTest(): string {
+    return this.appService.getHello() + ' test';
+  }
 }
