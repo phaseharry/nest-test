2dcc536d24a61a37eee371c7b23613b56d278a34
Harry <harrychenhc@icloud.com>
2021-02-24 14:33:41 -0500

    playing with routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
 
+// To create controllers, you can use the cli command
+// nest g controller cats (to create a cats.controller.ts)
 @Controller()
+// required Controller that controls the request and response of a request
+// can pass in an optional argument into controller to specify a path
+// ex) @Controller('cats') would control requests that are prepended by "/cats".
+// else if no parameter was passed it, it will be "/"
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  // Decorator for the "GET" request
+  @Get() // gets called when the "/" path is hit. (Calls the getAllCats method)
+  @HttpCode(201) // manually changing a response status code
+  getAllCats(): string {
+    return 'this gets all cats';
+  }
+
+  @Get('/@res') // for get request, default is 200
+  usingRes(@Res() response: Response): Response {
+    // injecting the response object from express to manually control it if we wish
+    return response.send('using @res from express');
+  }
+
+  @Get('/hello') // gets called when the "/hello" path is hit
+  getTest(): string {
     return this.appService.getHello();
   }
 
-  @Get('/test-route')
-  getTest(): string {
-    return this.appService.getHello() + ' test';
+  @Post()
+  createCat(): string {
+    return 'post request used to create a new cat';
   }
 }
